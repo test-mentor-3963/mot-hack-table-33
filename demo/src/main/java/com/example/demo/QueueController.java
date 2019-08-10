6e287b04516cb0ff0c6050ce5490c74a13bc2f87
Shashi2409 <shashi.k2409@gmail.com>
2019-08-10 11:54:21 +0530

    First Commit

---
+++ demo/src/main/java/com/example/demo/QueueController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueController {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(QueueController.class, args);
+		
+		
+	}
+
+}
