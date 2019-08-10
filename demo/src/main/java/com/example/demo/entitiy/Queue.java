acddb5ae59c01fc338369815b2fe502ac2a249a3
user <user@DESKTOP-SC1SSM4>
2019-08-10 00:26:36 -0700

    entity structure -teju

--- demo/src/main/java/com/example/demo/entitiy/Queue.java
+++ demo/src/main/java/com/example/demo/entitiy/Queue.java
@@ -3,10 +3,29 @@
  */
 package com.example.demo.entitiy;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author user
  *
  */
+@Entity
+@Table(name="QUEUE")
 public class Queue {
+	@Id
+	@Column(name = "QUEUE_ID")
+	private long queueId;
+	
+	@Column(name = "QUEUE_NAME")
+	private String queueName;
+	
+	@Column(name = "QUEUE_MAX_LIMIT")
+	private String queueMaxLimit;
+	
+	@Column(name = "ADDED_TIME")
+	private String addedTime;
 
 }
